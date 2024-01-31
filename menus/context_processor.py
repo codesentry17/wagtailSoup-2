39aa52b319df57db22f4debd64a413fa682795e5
scrappy <scrappycoco902@gmail.com>
2024-01-31 21:44:15 +0530

    TranslatableMixin Snippet Sorted

--- menus/context_processor.py
+++ menus/context_processor.py
@@ -1,76 +1,12 @@
-from .models import NavTab, Navbar
-from parler.utils.context import switch_language
-
+from .models import Navbar
+from wagtail.models import Locale
 
 def get_nav_tab(request):
-    current_language = request.LANGUAGE_CODE
-    
-    if current_language == 'en':
-        if Navbar.objects.exists():
-            navbar = Navbar.objects.first()
-            return {
-                'WebName1':navbar.name1,
-                'WebName2':navbar.name2,
-                'tabs':navbar.nav_tab.all()    
-            }
-        else:
-            return {
-                'WebName1':"Navbar",
-                'WebName2':"Deleted",
-                'tabs':None
-            }
-        
-    elif current_language == 'mr':
-        if Navbar.objects.exists():
-            navbar = Navbar.objects.get(id=2)
-            return {
-                'WebName1':navbar.name1,
-                'WebName2':navbar.name2,
-                'tabs':navbar.nav_tab.all()    
-            }
-        else:
-            return {
-                'WebName1':"Navbar",
-                'WebName2':"Deleted",
-                'tabs':None
-            }
 
+    navbar = Navbar.objects.get(locale=Locale.get_active())
 
-
-    # name = Navbar.objects.first()
-    # if NavTab.objects.exists():
-    #     tabs = NavTab.objects.all()
-    #     return {
-    #         'WebName1':name.name1,
-    #         'WebName2':name.name2,
-    #         'tabs':tabs
-    #     }
-
-
-
-"""
-{% if tabs %}
-
-{% for tab in tabs %}
-
-{% if tab.page_list.count() < 2 %}
-    single page link
-
-    name:nav_title
-    url:tab.page_list.first().page_link.slug
-{% else %}
-    dropdown
-
-    name:nav_title
-    for page in tab.page_list.all():
-        name:page.page_link.title
-        url:page.page_link.slug
-{% endif %}
-
-
-{% endfor %}
-
-
-{% endif %}
-
-"""
+    return {
+        'WebName1':navbar.name1,
+        'WebName2':navbar.name2,
+        'tabs':navbar.nav_tab.all()
+    }
